71819700eec4a7f7fec36e32a67c3f444c5c5b2c
nf-rp-efrei <nicolas.flasque@efrei.net>
2026-09-09 16:59:19 +0200

    last updates

--- TI301_TP1/predefined_lists/src/main.c
+++ TI301_TP1/predefined_lists/src/main.c
@@ -10,15 +10,15 @@ int main()
     // initialize it with an empty list
 
      // visualize the list with the displayStdList function
-    displayStdList(SampleStdList_2());
-    // pour avoir une visualisation, ficher svg dans le dossier output
-    listToSvg(SampleStdList_2() ,"empty_list.svg");
+
+    // create a svg file in the output directory to have a pretty diagram
+    listToSvg(votre_variable_liste ,"empty_list.svg");
 
     // initialize the list with a sample list (SampleStdList_1)
 
     // visualize the list with the displayStdList function
 
-    // pour avoir une visualisation, ficher svg dans le dossier output
-    //listToSvg(votre_variable_liste ,"sample_list_1.svg");
+    // create a svg file in the output directory to have a pretty diagram
+    listToSvg(votre_variable_liste ,"sample_list_1.svg");
     return 0;
 }
